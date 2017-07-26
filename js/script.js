a46720645ec07b8d8c79561c51cbe609f6321fe1
Igor Igeto Mitkovski <igeto@users.noreply.github.com>
2017-07-26 17:06:29 +0200

    changes 26.07.2017

--- js/script.js
+++ js/script.js
@@ -15,14 +15,22 @@ $(window).scroll(function() {
         $(".html").addClass("html_progress-bar");
         $(".css").addClass("css_progress-bar");
         $(".js").addClass("js_progress-bar");
+        $(".csharp").addClass("csharp_progress-bar");
+        $(".sql").addClass("sql_progress-bar");
         $(".bs").addClass("bs_progress-bar");
         $(".jquery").addClass("jquery_progress-bar");
+        $(".mvc").addClass("mvc_progress-bar");
+        $(".api").addClass("api_progress-bar");
     } else {
         $(".html").removeClass("html_progress-bar");
         $(".css").removeClass("css_progress-bar");
         $(".js").removeClass("js_progress-bar");
+        $(".csharp").removeClass("csharp_progress-bar");
+        $(".sql").removeClass("sql_progress-bar");
         $(".bs").removeClass("bs_progress-bar");
         $(".jquery").removeClass("jquery_progress-bar");
+        $(".mvc").removeClass("mvc_progress-bar");
+        $(".api").removeClass("api_progress-bar");
     }
 });
 
